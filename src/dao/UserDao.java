d85f75d0e448358a4b269b0fcf62c7d0598e4889
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-10 13:08:09 +0900

    Update UserDao.java

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -8,8 +8,9 @@ import java.sql.SQLException;
 
 import model.User;
 
-public class UserDao {
-	public boolean isLoginOK(User idpw) {
+public class UserDAO {
+	// ログインできるならtrueを返す
+	public boolean isLoginOK(User user) {
 		Connection conn = null;
 		boolean loginResult = false;
 
@@ -18,13 +19,13 @@ public class UserDao {
 			Class.forName("org.h2.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/D1", "sa", "");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
 
 			// SELECT文を準備する
 			String sql = "SELECT COUNT(*) FROM Idpw WHERE id = ? AND pw = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			pStmt.setString(1, idpw.getId());
-			pStmt.setString(2,idpw.getPw());
+			pStmt.setString(1, user.getId());
+			pStmt.setString(2,user.getPw());
 
 			// SELECT文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
@@ -59,5 +60,60 @@ public class UserDao {
 		// 結果を返す
 		return loginResult;
 	}
-}
 
+	// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
+	public boolean insert(User card) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+
+			// SQL文を準備する（AUTO_INCREMENTのNUMBER列にはNULLを指定する）
+			String sql = "INSERT INTO Idpw VALUES (?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (card.getId() != null && !card.getId().equals("")) {
+				pStmt.setString(1, card.getId());
+			}
+			else {
+				pStmt.setString(1, "（未設定）");
+			}
+			if (card.getPw() != null && !card.getPw().equals("")) {
+				pStmt.setString(2, card.getPw());
+			}
+			else {
+				pStmt.setString(2, "（未設定）");
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 結果を返す
+		return result;
+	}
+}
