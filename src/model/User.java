0b1ab0519e69f7dccd3574caad2a3f7b9f8f7764
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-11 13:36:16 +0900

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,58 +1,55 @@
 package model;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class User implements Serializable {
-	private int id;	// ID
-	private String user_id;	// User_ID
-	private String user_name; // User_Name
+	private String login_id;	// ID
+	private String user_name; // 氏名
 	private String password; //PassWord
-	private int n_question; //質問数
-	private int n_answer; // 質問回答数
-	private String s_content; // 質問内容（生徒間）
-	private int s_answer; // 質問回答（生徒間）
-	private String t_content; // 質問内容（教師）
-	private int t_answer; // 質問回答（教師）
+	private String content; // 質問内容（生徒間）
+	private int answer; // 質問回答（生徒間）
+	private LocalDateTime date;
 	private String subject; //科目
 	private int score; //得点
 
 
 
-	public User(String user_id, String password) {
-		this.user_id = user_id;
+	public User(String login_id, String password) {
+		this.login_id = login_id;
 		this.password = password;
 	}
 
 	public User() {
-		this.user_id = "";
+		this.login_id = "";
 		this.password = "";
 	}
 
 	/**
 	 * @return id
 	 */
-	public int getId() {
-		return id;
+	public String getId() {
+		return login_id;
 	}
 
 	/**
 	 * @param id セットする id
 	 */
-	public void setId(int id) {
-		this.id = id;
+	public void setId(String id) {
+		this.login_id = id;
 	}
 
 	/**
 	 * @return user_id
 	 */
 	public String getUser_id() {
-		return user_id;
+		return login_id;
 	}
 
 	/**
 	 * @param user_id セットする user_id
 	 */
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
+	public void setUser_id(String login_id) {
+		this.login_id = login_id;
 	}
 
 	/**
@@ -82,89 +79,32 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	/**
-	 * @return n_question
-	 */
-	public int getN_question() {
-		return n_question;
-	}
-
-	/**
-	 * @param n_question セットする n_question
-	 */
-	public void setN_question(int n_question) {
-		this.n_question = n_question;
-	}
-
-	/**
-	 * @return n_answer
-	 */
-	public int getN_answer() {
-		return n_answer;
-	}
-
-	/**
-	 * @param n_answer セットする n_answer
-	 */
-	public void setN_answer(int n_answer) {
-		this.n_answer = n_answer;
-	}
-
 	/**
 	 * @return s_content
 	 */
-	public String getS_content() {
-		return s_content;
+	public String getContent() {
+		return content;
 	}
 
 	/**
 	 * @param s_content セットする s_content
 	 */
-	public void setS_content(String s_content) {
-		this.s_content = s_content;
+	public void setContent(String content) {
+		this.content = content;
 	}
 
 	/**
 	 * @return s_answer
 	 */
-	public int getS_answer() {
-		return s_answer;
+	public int getAnswer() {
+		return answer;
 	}
 
 	/**
 	 * @param s_answer セットする s_answer
 	 */
-	public void setS_answer(int s_answer) {
-		this.s_answer = s_answer;
-	}
-
-	/**
-	 * @return t_content
-	 */
-	public String getT_content() {
-		return t_content;
-	}
-
-	/**
-	 * @param t_content セットする t_content
-	 */
-	public void setT_content(String t_content) {
-		this.t_content = t_content;
-	}
-
-	/**
-	 * @return t_answer
-	 */
-	public int getT_answer() {
-		return t_answer;
-	}
-
-	/**
-	 * @param t_answer セットする t_answer
-	 */
-	public void setT_answer(int t_answer) {
-		this.t_answer = t_answer;
+	public void setAnswer(int answer) {
+		this.answer = answer;
 	}
 
 	/**
@@ -195,5 +135,18 @@ public class User implements Serializable {
 		this.score = score;
 	}
 
+	/**
+	 * @return score
+	 */
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	/**
+	 * @param score セットする score
+	 */
+	public void setScore(LocalDateTime date) {
+		this.date = date;
+	}
 
 }
