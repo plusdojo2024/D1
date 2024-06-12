5b73ff690254181f656f3533b9dfc618565a3d85
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 10:36:22 +0900

    Update ScoreRegistServlet.java

--- src/servlet/ScoreRegistServlet.java
+++ src/servlet/ScoreRegistServlet.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.UserDao;
-import model.Bc;
 import model.Result;
+import model.grade;
 
 /**
  * Servlet implementation class ScoreRegistServlet
@@ -20,21 +20,20 @@ import model.Result;
 @WebServlet("/ScoreRegistServlet")
 public class ScoreRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ScoreRegistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/D1/LoginServlet");
+			return;
+	}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
@@ -43,31 +42,30 @@ public class ScoreRegistServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトするh
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/simpleBC/LoginServlet");
+			response.sendRedirect("/D1/LoginServlet");
 			return;
 
 			// リクエストパラメータを取得する
 			request.setCharacterEncoding("UTF-8");
 			// 改造（ここから）
-			String company = request.getParameter("company");
-			String department = request.getParameter("department");
-			String position = request.getParameter("position");
-			String name = request.getParameter("name");
+			String company = request.getParameter("score");
+
 			// 改造（ここまで）
 
 			// 登録処理を行う
-			UserDao UserDao = new UserDao();
+			UserDao UDao = new UserDao();
 			// 改造（ここから）
-			if (UserDao.insert(new Bc(0, company, department, position, name))) {	// 登録成功
+			if (UDao.insert(new grade(score))) {	// 登録成功
 				// 改造（ここまで）
 				request.setAttribute("result",
-				new Result("登録成功！", "レコードを登録しました。", "/simpleBC/MenuServlet"));
+				new Result("登録成功！", "成績を登録しました。", "/D1/HomeServlet"));
 			}
 			else {												// 登録失敗
 				request.setAttribute("result",
-				new Result("登録失敗！", "レコードを登録できませんでした。", "/simpleBC/MenuServlet"));
+				new Result("登録失敗！", "成績を登録できませんでした。", "/D1/HomeServlet"));
 			}
 
 			// 結果ページにフォワードする
