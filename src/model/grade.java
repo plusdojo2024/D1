f0906e054de7e944231483b903513e97984ac2b6
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 11:15:38 +0900

    Update grade.java

--- src/model/grade.java
+++ src/model/grade.java
@@ -8,69 +8,45 @@ public class grade {
 	private String subject; //科目
 	private int score; //得点
 
-
-
-	public grade(String login_id) {
+	public grade(String login_id,LocalDateTime date,String subject,int score) {
+		super();
 		this.login_id = login_id;
+		this.date = date;
+		this.subject = subject;
+		this.score = score;
 	}
 
 	public grade() {
-		this.login_id = "";
 	}
 
-	/**
-	 * @return login_id
-	 */
 	public String getLogin_id() {
 		return login_id;
 	}
 
-	/**
-	 * @param login_id セットする login_id
-	 */
 	public void setLogin_id(String login_id) {
 		this.login_id = login_id;
 	}
 
-
-	/**
-	 * @return subject
-	 */
 	public String getSubject() {
 		return subject;
 	}
 
-	/**
-	 * @param subject セットする subject
-	 */
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 
-	/**
-	 * @return score
-	 */
 	public int getScore() {
 		return score;
 	}
 
-	/**
-	 * @param score セットする score
-	 */
 	public void setScore(int score) {
 		this.score = score;
 	}
 
-	/**
-	 * @return date
-	 */
 	public LocalDateTime getDate() {
 		return date;
 	}
 
-	/**
-	 * @param date セットする date
-	 */
 	public void setScore(LocalDateTime date) {
 		this.date = date;
 	}
