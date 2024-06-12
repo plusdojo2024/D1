95143a3326cbbfc6baf2f66c4b59885b4529952f
plusdojo2024-kazama <kazama-ikko-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 15:09:41 +0900

    Update HomeServlet.java

--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MenuServlet
@@ -22,11 +21,11 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("login_id") == null) {
-			response.sendRedirect("/D1/LoginServlet");
-			return;
-		}
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("login_id") == null) {
+//			response.sendRedirect("/D1/LoginServlet");
+//			return;
+//		}
 
 		// ホームページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp");
