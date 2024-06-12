c689619af50379f281046475dd27f1578714f3ed
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 14:28:17 +0900

    Update grade.java

--- src/model/grade.java
+++ src/model/grade.java
@@ -1,17 +1,21 @@
 package model;
 
-import java.time.LocalDateTime;
+import java.sql.Time;
+import java.util.Date;
+
 
 public class grade {
 	private String login_id;	// ID
-	private LocalDateTime date;//日付
+	private Date date;//日付
+	private Time time;//時間
 	private String subject; //科目
 	private int score; //得点
 
-	public grade(String login_id,LocalDateTime date,String subject,int score) {
+	public grade(String login_id,Date date,Time time,String subject,int score) {
 		super();
 		this.login_id = login_id;
 		this.date = date;
+		this.time=time;
 		this.subject = subject;
 		this.score = score;
 	}
@@ -43,12 +47,20 @@ public class grade {
 		this.score = score;
 	}
 
-	public LocalDateTime getDate() {
+	public Date getDate() {
 		return date;
 	}
 
-	public void setDate(LocalDateTime date) {
+	public void setDate(Date date) {
 		this.date = date;
+	}
+
+	public Time getTime() {
+		return time;
+	}
+
+	public void setTime(Time time) {
+		this.time = time;
 	}
 
 }
