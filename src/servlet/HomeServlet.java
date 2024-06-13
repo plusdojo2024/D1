d56bda079ec19a6d27ba05b1eac63d6457727029
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 17:01:34 +0900

    Update HomeServlet.java

--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -1,6 +1,13 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -26,7 +33,62 @@ public class HomeServlet extends HttpServlet {
 //            return;
 //        }
 
-		// リクエストパラメータを取得する
+		Connection conn = null;
+        List<String> cardList = new ArrayList<>();
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/D1", "sa", "");
+				Statement st = conn.createStatement();
+				 //PreparedStatement statement = conn.prepareStatement(下のクエリ);エラーがでたら追加
+				//SQLのクエリ（Homeサーブレットはすべてのテーブルからデータを取得するのですべて結合してから取得）
+				ResultSet res = st.executeQuery("SELECT User.login_id, User.user_name, User.password, "
+						+ "Grade.score, Question.date, Question.content, Question.answer, Question.subject "
+						+ "FROM User INNER JOIN Grade ON login_id = Grade.login_id "
+						+ "INNER JOIN Question ON User.login_id = Question.login_id");
+
+				// login_id,user_name,passwordカラム（Userテーブル）のデータを取得するループ
+				//以下のwhile処理はResultSetが次の行に移動し、その行が存在する限り実行するので行が統一されているカラム同士を分けてる
+				while (res.next()) {
+				    String loginId = res.getString("login_id");
+				    String userName = res.getString("user_name");
+				    String password = res.getString("password");
+
+				}
+
+				// scoreカラム（Gradeテーブル）のデータを取得するループ
+				res.beforeFirst(); // ResultSetを最初の行の前に移動する
+				while (res.next()) {
+				    int score = res.getInt("score");
+
+				}
+
+				// date,content,subjectカラム（Questionテーブル）のデータを取得するループ
+				res.beforeFirst();
+				while (res.next()) {
+				    String date = res.getString("date");
+				    String content = res.getString("content");
+				    String subject = res.getString("subject");
+				}
+
+				// answerカラム（Questionテーブル）のデータを取得するループ
+				//　answerとcontentが同じ行数とは限らないのでループを分けました。
+				res.beforeFirst();
+				while (res.next()) {
+				    String answer = res.getString("answer");
+				}
+
+			 } catch(SQLException e) {
+		            e.printStackTrace();
+		    }
+
+		    request.setAttribute("books", books);
+
+
+				// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		String login_id = request.getParameter("login_id");
 		String user_name = request.getParameter("user_name");
@@ -35,7 +97,6 @@ public class HomeServlet extends HttpServlet {
 		String content = request.getParameter("content");
 		String answer = request.getParameter("answer");
 		String subject = request.getParameter("subject");
-		String time = request.getParameter("time");
 		String score = request.getParameter("score");
 
 		//パラメータをリクエスト属性として設定する
@@ -46,7 +107,6 @@ public class HomeServlet extends HttpServlet {
 		request.setAttribute("content", content);
 		request.setAttribute("answer", answer);
 		request.setAttribute("subject", subject);
-		request.setAttribute("time", time);
 		request.setAttribute("score", score);
 
 
@@ -55,7 +115,7 @@ public class HomeServlet extends HttpServlet {
         // ホームページにフォワードする
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp");
         dispatcher.forward(request, response);
-    }
+			}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
