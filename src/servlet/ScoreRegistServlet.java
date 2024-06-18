3ae8c5d652eb5813a83d46f5850276e5730038e6
plusdojo2024-asai <asai-kazuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 16:50:20 +0900

    updatescoreregistservlet

--- src/servlet/ScoreRegistServlet.java
+++ src/servlet/ScoreRegistServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.gradeDao;
 import model.Result;
@@ -33,18 +32,13 @@ public class ScoreRegistServlet extends HttpServlet {
 //			response.sendRedirect("/D1/LoginServlet");
 //			return;
 //	}
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ScoreRegist.jsp");
-
 		/*		response.getWriter().append("Served at: ").append(request.getContextPath());
 				HttpSession session = request.getSession();
 				if (session.getAttribute("id") == null) {
 					response.sendRedirect("/D1/LoginServlet");
 					return;
 			}*/
-
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp")
-		//RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp");
-
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ScoreRegist.jsp");
 		dispatcher.forward(request, response);
 }
 
@@ -53,37 +47,30 @@ public class ScoreRegistServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-		// もしもログインしていなかったらログインサーブレットにリダイレクトするh
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/D1/LoginServlet");
-			return;
+
+		request.setCharacterEncoding("UTF-8");
+		String subject = request.getParameter("subject");
+		//int型は一旦string型でパラメーターを受け取ってそれをint型に直す必要がある
+		String scoreString = request.getParameter("score");
+		int score = Integer.parseInt(scoreString);
+
+		gradeDao gDao = new gradeDao();
+
+		if (gDao.insert(new grade(null,null,null,subject,score))) {	// 登録成功
+
+			request.setAttribute("result",
+			new Result("登録成功！", "成績を登録しました。", "/D1/HomeServlet"));
+		}
+		else {												// 登録失敗
+			request.setAttribute("result",
+			new Result("登録失敗！", "成績を登録できませんでした。", "/D1/ScoreRegistServlet"));
 		}
 
-			request.setCharacterEncoding("UTF-8");
-			String subject = request.getParameter("subject");
-			//int型は一旦string型でパラメーターを受け取ってそれをint型に直す必要がある
-			String scoreString = request.getParameter("score");
-			int score = Integer.parseInt(scoreString);
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Result.jsp");
+		dispatcher.forward(request, response);
 
-			gradeDao gDao = new gradeDao();
-
-			if (gDao.insert(new grade(null,null,null,subject,score))) {	// 登録成功
-
-				request.setAttribute("result",
-				new Result("登録成功！", "成績を登録しました。", "/D1/HomeServlet"));
-			}
-			else {												// 登録失敗
-				request.setAttribute("result",
-				new Result("登録失敗！", "成績を登録できませんでした。", "/D1/HomeServlet"));
-			}
-
-			// 結果ページにフォワードする
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
-			dispatcher.forward(request, response);
-
-		}
 	}
+}
 
 
