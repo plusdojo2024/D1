4b83dd432e3a381fcae685852f87972ad1a596a0
plusdojo2024-Enomoto <enomoto-keisuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 14:08:30 +0900

    グラフ

--- WebContent/WEB-INF/jsp/Home.jsp
+++ WebContent/WEB-INF/jsp/Home.jsp
@@ -40,24 +40,8 @@
 
 		<script>
 		const array1 = [
-			  <c:forEach var="item" items="${test1}">
-			     ${item},
-			  </c:forEach>];
-		 const array2 = [
-			  <c:forEach var="item1" items="${test2}">
-				   ${item1},
-			  </c:forEach>];
-		const array3 = [
-			  <c:forEach var="item2" items="${test3}">
-				   ${item2},
-			  </c:forEach>];
-		const array4 = [
-			  <c:forEach var="item3" items="${test4}">
-				   ${item3},
-			  </c:forEach>];
-		const array5 = [
-			  <c:forEach var="item4" items="${test5}">
-				  ${item4},
+			  <c:forEach var="entry" items="${averageScores}">
+			  	${entry.averageScore},
 			  </c:forEach>];
 		</script>
 
--- WebContent/script/Line_Chart.js
+++ WebContent/script/Line_Chart.js
@@ -10,33 +10,9 @@ var myLineChart = new Chart(ctx, {
 	 labels: ['4月','5月','6月','7月','9月','10月','11月','12月','1月','2月','3月'],
 	 datasets: [
      {
-	   label: '国語',
+	   label: '成績推移',
 	   data: array1,
 	   borderColor: "rgba(255,0,0,1)",
-	   backgroundColor: "rgba(0,0,0,0)"
-	 },
-	 {
-	   label: '数学',
-	   data: array2,
-	   borderColor: "rgba(0,0,255,1)",
-	   backgroundColor: "rgba(0,0,0,0)"
-	  },
-	 {
-	   label: '英語',
-	   data: array3,
-	   borderColor: "rgba(0,128,0,1)",
-	   backgroundColor: "rgba(0,0,0,0)"
-	 },
-	 {
-	   label: '理科',
-	   data: array4,
-	   borderColor: "rgba(255,255,0,1)",
-	   backgroundColor: "rgba(0,0,0,0)"
-	 },
-	 {
-	   label: '社会',
-	   data: array5,
-	   borderColor: "rgba(255,192,203,1)",
 	   backgroundColor: "rgba(0,0,0,0)"
 	 },
 	   ],
