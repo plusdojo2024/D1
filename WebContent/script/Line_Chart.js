17a6edd053580413f9fd6e64e627d428ddab45a9
plusdojo2024-Enomoto <enomoto-keisuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 09:48:20 +0900

    グラフ年度月表示

--- WebContent/script/Line_Chart.js
+++ WebContent/script/Line_Chart.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myLineChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
    data: {
-	 labels: ['4月','5月','6月','7月','9月','10月','11月','12月','1月','2月','3月'],
+	 labels: array2,
 	 datasets: [
      {
 	   label: '成績推移',
