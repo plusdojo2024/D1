734b7d3c4cbd50df6916835ecefed673d6709d87
plusdojo2024-jinguji <jinguji-rina-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 11:44:43 +0900

    Update StudentQueSubResultServlet.java

--- src/servlet/StudentQueSubResultServlet.java
+++ src/servlet/StudentQueSubResultServlet.java
@@ -1,6 +1,13 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,10 +15,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import dao.QuestionDao;
-import model.Question;
-import model.Result;
 /**
  * Servlet implementation class StudentQueSubServlet
  */
@@ -19,19 +22,73 @@ import model.Result;
 public class StudentQueSubResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public StudentQueSubResultServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+        request.setCharacterEncoding("UTF-8");
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/D1", "sa", "");
+			//SQLのクエリ（Homeサーブレットはすべてのテーブルからデータを取得するのですべて結合してから取得）
+			String sql = "SELECT user_name FROM User";
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			List<String> names = new ArrayList<>();
+			List<String> contents = new ArrayList<>();
+			List<String> answers = new ArrayList<>();
+
+			while (res.next()) {
+			    String userName = res.getString("user_name");
+			    names.add(userName);
+			}
+			request.setAttribute("names", names);
+
+			String sql2 = "SELECT content FROM Question";
+			PreparedStatement st2 = conn.prepareStatement(sql2);
+			ResultSet res2 = st2.executeQuery();
+
+			while (res2.next()) {
+				String content = res2.getString("content");
+				contents.add(content);
+			}
+
+			request.setAttribute("contents", contents);
+
+
+			// answerカラム（Questionテーブル）のデータを取得するループ
+			//　answerとcontentが同じ行数とは限らないのでループを分けました。
+			String sql3 = "SELECT answer FROM Question";
+			PreparedStatement st3 = conn.prepareStatement(sql3);
+			ResultSet res3 = st3.executeQuery();
+
+			while (res3.next()) {
+		        String answer = res3.getString("answer");
+		        answers.add(answer);
+			}
+			request.setAttribute("answers", answers);
+			  st.close();
+			    res.close();
+			    st2.close();
+			    res2.close();
+			    st3.close();
+			    res3.close();
+			    conn.close();
+
+
+		 } catch(SQLException e) {
+	            e.printStackTrace();
+	    }catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    }
+
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/StudentQueSubResult.jsp");
 		dispatcher.forward(request, response);
@@ -45,23 +102,7 @@ public class StudentQueSubResultServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		doGet(request, response);
 
-		String login_id=request.getParameter("login_id");
-		String date=request.getParameter("date");
-		String content=request.getParameter("content");
-		String answer=request.getParameter("answer");
-		String subject=request.getParameter("subject");
 
-		QuestionDao qDao=new QuestionDao();
-
-
-		if(qDao.insert(new Question(login_id, date, content, answer, subject))) {
-			request.setAttribute("result",
-					new Result("登録完了","質問又は回答を受け付けました！","/D1/StudentQueSubResultServlet"));
-		}
-		else {
-			request.setAttribute("result",
-					new Result("登録失敗", "質問又は回答を受け付けられませんでした。","/D1/StudentQueSubResultServlet"));
-		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/StudentQueSubResult.jsp");
 		dispatcher.forward(request, response);
 	}
