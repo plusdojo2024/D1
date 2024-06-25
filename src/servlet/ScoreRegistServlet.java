fb0703f4d5268034dc91fe8024987e9be8f00320
plusdojo2024-hayashi <hayashi-ikuya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-25 15:31:44 +0900

    Update ScoreRegistServlet.java

--- src/servlet/ScoreRegistServlet.java
+++ src/servlet/ScoreRegistServlet.java
@@ -57,15 +57,23 @@ public class ScoreRegistServlet extends HttpServlet {
 		System.out.println("Received score: " + score);
 		System.out.println("Received subject: " + subject);
 
+		if(login_id.isEmpty() || date.isEmpty() || score==0 || subject.isEmpty()) {
+			request.setAttribute("result",
+			new Result("登録失敗！", "全ての項目を入力してください。", "/D1/StudentQueSubResultServlet"));
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/Result.jsp");
+			dispatcher.forward(request, response);
+			return;
+		}
+
 		gradeDao gDao = new gradeDao();
 
 		if (gDao.insert(new grade(login_id, date, subject, score))) { // 登録成功
 
 			request.setAttribute("result",
-					new Result("登録成功！", "成績を登録しました。", "/D1/HomeServlet"));
+					new Result("登録成功！", "成績を登録しました。", "/WEB-INF/jsp/Result.jsp"));
 		} else { // 登録失敗
 			request.setAttribute("result",
-					new Result("登録失敗！", "成績を登録できませんでした。", "/D1/ScoreRegistServlet"));
+					new Result("登録失敗！", "成績を登録できませんでした。", "/WEB-INF/jsp/Result.jsp"));
 		}
 
 		// 結果ページにフォワードする
