405937aef7ad3c204d6eb1b6a115b0a873534956
plusdojo2024-kazama <kazama-ikko-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-26 14:42:18 +0900

    Update StudentQueSubResult_insideServlet.java

--- src/servlet/StudentQueSubResult_insideServlet.java
+++ src/servlet/StudentQueSubResult_insideServlet.java
@@ -1,10 +1,12 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +19,7 @@ import model.Question;
  * Servlet implementation class StudentQueSubServlet
  */
 @WebServlet("/StudentQueSubResult_insideServlet")
+@MultipartConfig
 public class StudentQueSubResult_insideServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -51,11 +54,13 @@ public class StudentQueSubResult_insideServlet extends HttpServlet {
 		String login_id = request.getParameter("login_id");
 		String content = request.getParameter("content");
 		String answer = request.getParameter("answer");
-		String subject = request.getParameter("subject");
+		String subject = request.getParameter("subject_se");
 		String reaction = request.getParameter("reaction");
 
 		AllQuestionDao QDao = new AllQuestionDao();
 		List<Question> QueList = QDao.select(new Question(login_id, date, content, answer, subject,reaction));
+
+		Collections.reverse(QueList);
 
 		request.setAttribute("QueList", QueList);
 
